323c7933d9532be90558af405954a35a28ca0664
elvis <elvina@safetycat.org>
2016-03-21 16:52:15 +0000

    UIkit override in bower.json add responsive menu

--- templates/header.php
+++ templates/header.php
@@ -3,6 +3,27 @@
 
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
 
+
+<!-- Phone menu -->
+    <button class="uk-button" data-uk-offcanvas="{target:'#mobmenu'}">Menu</button>
+    <div class="uk-offcanvas" id="mobmenu">
+    <nav class="uk-offcanvas-bar uk-offcanvas-bar-flip">
+      <?php
+      if (has_nav_menu('primary_navigation')) :
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'items_wrap' => '<ul class="uk-nav uk-nav-offcanvas" data-uk-nav>%3$s</ul>']);
+      endif;
+      ?>
+
+      <?php
+      if (has_nav_menu('right_navigation')) :
+        wp_nav_menu(['theme_location' => 'right_navigation', 'menu_class' => 'uk-offcanvas-bar']);
+      endif;
+      ?>
+    </nav>
+  </div>
+<!-- Phone menu -->
+
+
     <nav class="nav-primary">
       <?php
       if (has_nav_menu('primary_navigation')) :
