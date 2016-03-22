d248678d52ffa8140e8d5b5ec3edd2147af2a4dd
elvis <elvina@safetycat.org>
2016-03-22 16:29:27 +0000

    menus on front page copyright in footer straplin in header

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,10 @@
 <footer class="content-info">
   <div class="container">
+  	<div>The Pathology Partnership is an NHS pathology provider </div>
   	<div class="cat-grid" data-uk-grid-match="{target:'.cat-panel'}">
     <?php dynamic_sidebar('sidebar-footer'); ?>
     </div>
+    <span class=""><?php printf( __( 'Copyright &copy; %s %s. All Rights Reserved.', 'sage' ), date( 'Y' ), ' <a href="' . home_url() . '">' . get_bloginfo( 'name' ) .'</a>' ); ?></span>
+Website by <a href="http://iwantcat.com/" title="I Want Cat">I Want Cat</a>
   </div>
 </footer>
