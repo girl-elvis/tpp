784129a7891982494007c7a59635ee9883294984
elvis <elvina@safetycat.org>
2016-03-23 15:16:37 +0000

    fixes on submenu and add fontawesome

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,25 +1,21 @@
 <?php use Roots\Sage\Titles; ?>
 
-<div class="page-header">
-  <h1><?= Titles\title(); ?></h1>
-
-
 <?php
+// Adds subnav to page header
 if (is_page("patients") || is_child( 'patients')) {
-	
 	 $menuParameters = array(
-
-		'menu' => 'Patients',
-		'before' => '<h3>',
-		'after'=>'</h3>',
-		//'items_wrap' => '%3$s', // removes UL
-		'echo' => false,
-
-	 	'menu_class' => 'cat-grid pagenav',		
+		'menu' => 'Patients', 'before' => '<i></i><h3>', 'after'=>'</h3>', 'echo' => false, 'menu_class' => 'cat-grid pagenav',		
 		);     
     echo (wp_nav_menu( $menuParameters ) );
 }
 
 ?>
+
+
+
+<div class="page-header">
+  <h1><?= Titles\title(); ?></h1>
+
+
 
 </div>
