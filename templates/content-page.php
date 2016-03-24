bc8d9089c654f8970ed08b6172795a4abc96f40d
James Cat <james@catplusplus.org.uk>
2016-03-24 16:53:25 +0000

    restore if on content-page that loads atop partial

--- templates/content-page.php
+++ templates/content-page.php
@@ -4,10 +4,14 @@
 <?php //wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
 
 
-
+<?php
+	if (is_page("a-z-of-tests") ){
+    	$post_type = "jargon";
+    	include(locate_template('templates/partial-atoz.php'));
+ 	}
+ ?>
 
 <?php if (is_page("staff") ){ ?>
-	
 
 <ul class="newspanels uk-grid uk-grid-match uk-grid-width-medium-1-3">
 <?php
@@ -19,23 +23,20 @@ $myposts = get_posts( $args );
 foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
 	<li><div class="cat-panel">
 		<div class="uk-panel-teaser">
-				<?php 
+				<?php
 		        if ( has_post_thumbnail() ) {
 
 					    the_post_thumbnail( 'cat-double' );
-					} 
-					
+					}
+
 
 	    echo ("</div><h4><a href='" . get_the_permalink() . "'>" . get_the_title() . "</a></h4>");
 	    ?>
 	</div></li>
-<?php endforeach; 
+<?php endforeach;
 wp_reset_postdata();?>
 
 </ul>
 
 
-
-
-	 
 <?php } ?>
