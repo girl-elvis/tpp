43e5b5ec75040afa14982549ecc257e49499fa12
James Cat <james@catplusplus.org.uk>
2016-03-24 16:06:29 +0000

    a-z tests fixed but there will be a problem if there is no entry for tests beginning with A

--- templates/partial-atoz.php
+++ templates/partial-atoz.php
@@ -4,46 +4,58 @@
 
 <div class="uk-accordion" data-uk-accordion>
 <?php
-$args = array( 'posts_per_page' => -1, 'post_type' => $post_type, 'status' => 'publish', 'order' => 'ASC', 'orderby' => 'name');
-$alphab = "A";
-$alphabprint = false;
+
+$args = array(
+	'posts_per_page'=> -1,
+	'post_type' 		=> $post_type,
+	'status' 				=> 'publish',
+	'order' 				=> 'ASC',
+	'orderby' 			=> 'name'
+);
+
+$state = array(
+	'letter'  	 => "",
+	'nextLetter' => "B",
+	'printed' 	 => false
+);
+
 $lastposts = get_posts( $args );
 
 foreach ( $lastposts as $post ) :
-	setup_postdata( $post ); 
-	$this_char = strtoupper(mb_substr($post->post_title, 0, 1, 'UTF-8'));
+    setup_postdata( $post );
 
-	if (!$alphabprint){
-			if ($alphab != "A") echo "</div>";
-			echo '<h3 class="uk-accordion-title">' . $alphab . '</h3>';
-			if ($alphab != "Z") echo '<div class="uk-accordion-content">';
-			$alphabprint = true;
-	}
+    $this_char = strtoupper(mb_substr($post->post_title, 0, 1, 'UTF-8'));
 
-	if ($alphab == $this_char ) { ?>
+    if ( $this_char != $state['letter'] ) {					// letter has changed
 
-	<div class="data-title"> <?php the_title(); ?></div>
-	<div class="data-definition"><?php the_content(); ?></div>
-
-	<?php
-	} else {
-		 $alphab++;
-		 $alphabprint = false;
-	}
-		
+        $state['letter'] = $this_char;							// update the letter to the new one
 
 
+        while ( $state['letter'] > $state['nextLetter'] ) {  // is the letter the next letter in the alphabet?
+        	echo '</div><h3 class="uk-accordion-title">' . $state['nextLetter'] . '</h3>';  // no, then print the accordian title
+        	echo '<div class="uk-accordion-content"><div class="data-definition">There is nothing here</div>'; // and accordian content saying nothing here
+        	$state['nextLetter']++;  // increment the next expected letter and go back to the top of this loop
+        } // PHP does not have while...else
+        if($state['letter'] == $state['nextLetter'] ) {
+        	$state['nextLetter']++;  // increment the next expected letter
+        }
 
-	
-		
-		 
+    		if ($this_char != "A") echo "</div>";   		// so close div unless it's the first change (!="A")
+        echo '<h3 class="uk-accordion-title">' . $this_char . '</h3>';  // print accordian title
+        echo '<div class="uk-accordion-content">';  // open the accordian content div
 
+
+    }
 ?>
-	
 
+    <div class="data-title"> <?php the_title(); ?></div>
+    <div class="data-definition"><?php the_content(); ?></div>
 
-<?php endforeach; 
-wp_reset_postdata(); 
+<?php
+
+endforeach;
+
+wp_reset_postdata();
 ?>
 
 </div>
