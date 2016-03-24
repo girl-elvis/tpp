db954c617c5fe53aad564c89bb54cb17bb943b4a
James Cat <james@catplusplus.org.uk>
2016-03-24 21:41:03 +0000

    fixed a-z listing so it can start with an empty letter, before it handled all empty letters except if they were at the beginning. now it can cope with empty letters anywhere. Currently the loop ends with the last entry in the a-z listing, so if your last entry is X you get no Y and Z entry.

--- templates/partial-atoz.php
+++ templates/partial-atoz.php
@@ -1,6 +1,20 @@
 <?php
 // Display the a to z list in accordian, alphabetically
+// this version curently bails out of the loop at the last entry, alphabetically
+// so for example if the last entry is X you get no Y and Z entry.
+
+    /* Prepare and print the index-letters */
+    echo '<div class="name_directory_index">';
+
+    $index_letters = range('A', 'Z');
+
+    foreach($index_letters as $index_letter)
+    {
+        echo ' <a class="accordian_opener" href="' . $letter_url . urlencode($index_letter) . '">' . strtoupper($index_letter) . '</a>  | ';
+    }
+    echo '</div>';
 ?>
+
 
 <div class="uk-accordion" data-uk-accordion>
 <?php
@@ -8,52 +22,55 @@
 $args = array(
 	'posts_per_page'=> -1,
 	'post_type' 		=> $post_type,
-	'status' 				=> 'publish',
-	'order' 				=> 'ASC',
+	'status' 			=> 'publish',
+	'order' 			=> 'ASC',
 	'orderby' 			=> 'name'
 );
 
 $state = array(
 	'letter'  	 => "",
-	'nextLetter' => "B",
-	'printed' 	 => false
+	'nextLetter' => "A",
+    'needsclose' => false  // boolean set to true if the accordian content div is open so we know to close it before writiing a new accordian title
 );
 
 $lastposts = get_posts( $args );
 
-foreach ( $lastposts as $post ) :
+foreach ( $lastposts as $post ) {
     setup_postdata( $post );
 
     $this_char = strtoupper(mb_substr($post->post_title, 0, 1, 'UTF-8'));
 
     if ( $this_char != $state['letter'] ) {					// letter has changed
+        $state['letter'] = $this_char;						// so update the current letter to the new one
 
-        $state['letter'] = $this_char;							// update the letter to the new one
 
-
-        while ( $state['letter'] > $state['nextLetter'] ) {  // is the letter the next letter in the alphabet?
-        	echo '</div><h3 class="uk-accordion-title">' . $state['nextLetter'] . '</h3>';  // no, then print the accordian title
-        	echo '<div class="uk-accordion-content"><div class="data-definition">There is nothing here</div>'; // and accordian content saying nothing here
+        // Loop to handle occasions when you have no entries for a letter
+        while ( $state['letter'] > $state['nextLetter'] ) {  // is the current letter beyond the next letter we were expecting (alphabetically) ?
+            if($state['needsclose'] == true) { echo '</div>'; $state['needsclose'] = false; } // yes, close the accordian content if it was open
+            echo '<h3 class="uk-accordion-title">' . $state['nextLetter'] . "</h3>\r";  // then print the accordian title for the current letter
+        	echo '<div class="uk-accordion-content">'; // print accordian content saying 'nothing here'
+            echo '  <div class="data-definition">There is nothing here</div>';
+            $state['needsclose'] = true; // remember to say that you just opened the accordian content div
         	$state['nextLetter']++;  // increment the next expected letter and go back to the top of this loop
-        } // PHP does not have while...else
+        } // we've looped enough times and created an 'nothing here' box for the expected next letters that were not there
+
         if($state['letter'] == $state['nextLetter'] ) {
-        	$state['nextLetter']++;  // increment the next expected letter
+        	$state['nextLetter']++;  // if the while loop just ran we need to increment the next expected letter 1 more...
         }
 
-    		if ($this_char != "A") echo "</div>";   		// so close div unless it's the first change (!="A")
+        // normal situation when new first letter matches expected next letter
+        if($state['needsclose'] == true) { echo '</div>'; $state['needsclose'] = false; } // close the accordian content if it is open
         echo '<h3 class="uk-accordion-title">' . $this_char . '</h3>';  // print accordian title
-        echo '<div class="uk-accordion-content">';  // open the accordian content div
-
-
+        echo '<div class="uk-accordion-content">';                      // open the accordian content div
+        $state['needsclose'] = true;
     }
+    // keep printing the post title and content in the loop until the letter changes again
 ?>
-
-    <div class="data-title"> <?php the_title(); ?></div>
-    <div class="data-definition"><?php the_content(); ?></div>
-
+    <div class="data-title">     <?php the_title();   ?></div><!-- close title-->
+    <div class="data-definition"><?php the_content(); ?></div><!-- close content -->
 <?php
 
-endforeach;
+}
 
 wp_reset_postdata();
 ?>
