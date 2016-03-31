f19922db815186f0e60fa792b87a1a0b569eaf67
blitz999 <nathan@artballs.co.uk>
2016-03-31 17:57:26 +0100

    Downloads sorted and media queried logo for test

--- templates/content-frontpage.php
+++ templates/content-frontpage.php
@@ -29,7 +29,7 @@
 		 	// loop through the rows of data
 		    while ( have_rows('boxes') ) : the_row();
 
-			echo "<div><div class='uk-panel uk-panel-box uk-panel-box-primary'><h3 class='uk-panel-title'>";
+			echo "<div><div class='uk-panel uk-panel-box uk-panel-box-secondary'><h3 class='uk-panel-title'>";
 
 			//echo "<div><div class='cat-panel cat-panel-primary'><h3 class='uk-panel-title'>";
 
